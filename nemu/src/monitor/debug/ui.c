558f12d022cd3f93908abbd1427af4389f0fc6ec
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-27 22:02:39 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:02:39 up  6:33,  1 user,  load average: 0.43, 0.38, 0.47 caa798069dbcea419dba16f715787e4b2b76b132

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,11 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+static int cmd_si(char *args){
+	cpu_exec(1);
+	return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,6 +51,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
+	{ "si", "dan bu zhi xing", cmd_si },
 
 	/* TODO: Add more commands */
 
