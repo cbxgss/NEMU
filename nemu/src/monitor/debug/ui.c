a54f2fa48e5318a8f2a3adc47e87f3bb702d3883
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-30 16:42:45 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:42:45 up 20 min,  1 user,  load average: 0.56, 0.61, 0.47 ac7a3aecf8a2cf98760b94f577207733baa917c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -46,8 +46,7 @@ static int info(char *args){
 	if(*args == 'r'){
 		int i;
 		for(i = 0; i < 8; i++){
-			printf("%s: 0X%X\t%s: 0X%X\n", 
-			regsl[i], cpu.gpr[i]._32, regsw[i], cpu.gpr[i]._16);
+			printf("%s: 0X%X\t%s: 0X%X\n", regsl[i], cpu.gpr[i]._32, regsw[i], cpu.gpr[i]._16);
 		}
 		for(i = 0; i < 4; i++){
 			printf("%s: 0X%X\t%s: 0X%X\n", regsb[i], cpu.gpr[i]._8[0], regsb[i+4], cpu.gpr[i]._8[1]);
