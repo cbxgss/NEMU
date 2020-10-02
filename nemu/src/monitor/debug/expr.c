b5a07622f44740f7dec4ae27f8c976ace9985e91
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-02 16:20:10 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:20:10 up  7:10,  1 user,  load average: 0.33, 0.49, 0.68 63a05d1c6065483397092a9bab2e3453bbbf0235

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -128,7 +128,18 @@ int check_parentheses(int p, int q) {	//对为1，括号匹配但两边无括号
 		}
 	}
 	if(num) return 0;
-	if(tokens[p].type == (int)'(' && tokens[q].type == (int)')') return 1;
+	if(tokens[p].type == (int)'(' && tokens[q].type == (int)')') {		//两边有()
+		int l = p+1; int num = 0;		//num = '('的个数 - ')'的个数
+		for(l = p; l < q; l++) {
+			if(tokens[l].type == (int)'(') num++;	//'('
+			else if(tokens[l].type == (int)')') {	//')'
+				if(!num) return 0;
+				else num--;
+			}
+			if(num < 0) return 2;
+		}
+		return 1;
+	}
 	return 2;
 }
 
