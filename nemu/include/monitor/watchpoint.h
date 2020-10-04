66b7e31f3f56565432051304ed90172b5d0706f1
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-04 11:34:41 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:34:41 up  2:59,  1 user,  load average: 1.41, 0.96, 0.78 92af9edc3fe0649939390208e4ae4882d1f520c7

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -6,10 +6,10 @@
 typedef struct watchpoint {
 	int NO;
 	struct watchpoint *next;
-
 	/* TODO: Add more members if necessary */
-
-
+	int value;		//旧值
+	int new_value;	//新值
+	char str[32];	//被监视的表达式
 } WP;
 
 #endif
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -20,4 +20,47 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+WP* new_wp(char *str_){
+	if(!free_) assert(0);
 
+	WP* now_wp = free_;		//第一个空闲wp
+	free_ = free_->next;
+
+	strcpy(now_wp->str, str_);
+	bool success; now_wp->value = expr(str_,&success);
+	now_wp->next = NULL;
+
+	if(!head){				//head为空
+		head = now_wp; now_wp->NO = 1;
+	}
+	else {
+		WP *qwq = head;
+		while (head->next) {	//qwq是上一个监视点
+			qwq = head->next;
+		}
+		now_wp->NO = qwq->NO + 1; qwq->next = now_wp;
+	}
+	printf("Number of watch is : %d\n", now_wp->NO);
+	return now_wp;
+}
+
+void free_wp(WP* wp){
+	if(head == wp){			//wp 是 第一个监视点
+		head = wp->next; wp->next = 0;
+		WP *qwq = free_;
+		while (qwq->next) qwq = qwq->next; 	//qwq是当前最后一个free_的wp，free_不可能为空
+		qwq->next = wp;
+	}
+	else {
+		WP *qwq = head;
+		while (qwq->next != wp) qwq = qwq->next; 	//qwq->next == wp
+		qwq->next = wp->next; wp->next = 0;
+
+		if(!free_) free_ = wp;
+		else {
+			qwq = free_;
+			while (qwq->next) qwq = qwq->next;		//qwq是当前最后一个free_的wp
+			qwq->next = wp;
+		}
+	}
+}
