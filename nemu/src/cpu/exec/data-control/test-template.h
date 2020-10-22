46e8342bebe508e6623527c832d2e9f3d779f63e
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-22 18:10:37 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:10:37 up 35 min,  1 user,  load average: 1.38, 1.09, 0.90 816beb5cb089051a1524de2d3c03b9b67a0a9850

--- nemu/src/cpu/exec/data-control/test-template.h
+++ nemu/src/cpu/exec/data-control/test-template.h
@@ -3,7 +3,18 @@
 #define instr test
 
 static void do_execute() {
-    
+    DATA_TYPE result = op_src->val & op_dest->val;      //两个操作数的&
+    cpu.CF = 0;                                         //检查无符号数溢出
+    cpu.OF = 0;                                         //检查有符号数溢出
+    cpu.SF = result >> (DATA_BYTE*8 -1);                // = 结果的符号位
+    cpu.ZF = !result;                                   // 结果==0 为1
+    int qwq = 0;
+    int i;
+    for(i = 0; i < 8; i++) qwq += (result >> i) & 1;// 结果的最低有效字节包含偶数个1 为1
+    if(qwq % 2) cpu.PF = 0;
+    else cpu.PF = 1;
+
+    print_asm_template2();
 }
 
 make_instr_helper(r2rm)
