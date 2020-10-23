05a65b44c6ee370ebfd5f2b3aadb728a77c55631
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-23 11:57:30 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:57:30 up  1:18,  1 user,  load average: 1.10, 0.83, 0.74 c173c141d3169afd9686c74a8541f06a3a43fe25

---
+++ nemu/src/cpu/exec/data-control/pop-template.h
@@ -0,0 +1,12 @@
+#include "cpu/exec/template-start.h"
+
+#define instr pop
+
+static void do_execute() {
+    swaddr_write(op_src->addr, 4, MEM_R(reg_l(R_SP)));
+    reg_l(R_SP) += 4;
+}
+
+make_instr_helper(r)
+
+#include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/data-control/push-template.h
+++ nemu/src/cpu/exec/data-control/push-template.h
@@ -3,8 +3,8 @@
 #define instr push
 
 static void do_execute() {
-        reg_l(R_SP) -= DATA_BYTE;
-        swaddr_write(reg_l(R_SP), DATA_BYTE, (DATA_TYPE_S)op_src->val);
+        reg_l(R_SP) -= 4;                       //为了pop方便，统一-4
+        swaddr_write(reg_l(R_SP), 4, (DATA_TYPE_S)op_src->val);
         print_asm_template1();
 }
 
