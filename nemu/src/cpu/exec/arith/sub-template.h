70393c40a97e99a210bf80daf71173f603f7418a
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-24 23:11:27 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:11:27 up 43 min,  1 user,  load average: 0.73, 0.81, 0.66 84e3401de8acb484383185f6ed9f70918ff6695f

---
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -0,0 +1,12 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sub
+
+static void do_execute() {
+    
+	print_asm_template2();
+}
+
+make_instr_helper(i2rm)
+
+#include "cpu/exec/template-end.h"
