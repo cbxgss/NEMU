b881f85fe9b9bbf66567ff26a3f09aef80b235de
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-25 10:30:06 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:30:06 up 12:02,  1 user,  load average: 0.97, 0.89, 0.82 1dc2c3461b75460ba1d6a41ec8928d49a69e24b2

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -4,33 +4,22 @@
 
 static void do_execute() {
 	DATA_TYPE result = op_dest->val - op_src->val;
-	OPERAND_W(op_dest, result);
-	//检查无符号数溢出
-	if(op_dest->val < op_src->val) cpu.CF = 1;
-    else cpu.CF = 0;
-	// = 结果的符号位
-    cpu.SF = result >> (DATA_BYTE*8 -1);
-    //检查有符号数溢出
-    int a = op_dest->val >> (DATA_BYTE*8 - 1);
-    int b = op_src->val >> (DATA_BYTE*8 - 1);
-		// a - b  = result
-		// +   -        -
-		// -   +        +
-		//因此，当a和b符号不同，结果和b的符号相同时，有符号数溢出
-    cpu.OF = (a != b) && (b == cpu.SF);
-    // 结果==0 为1
-    cpu.ZF = !result;
-	// 结果的最低有效字节包含偶数个1 为1
-    int qwq = 0;
-    int i;
-    for(i = 0; i < 8; i++) qwq += (result >> i) & 1;
-    if(qwq % 2) cpu.PF = 0;
-    else cpu.PF = 1;
-    //D3到D4是否进位
-    if(( (op_dest->val & 0xf) - (op_src->val & 0xf) ) >> 4) cpu.AF = 1;
-    else cpu.AF = 0;
-	print_asm_template2();
+	int len = (DATA_BYTE << 3) - 1;
+	cpu.CF = op_dest->val < op_src->val;
+	cpu.SF=result >> len;
+    	int s1,s2;
+	s1=op_dest->val>>len;
+	s2=op_src->val>>len;
+    	cpu.OF=(s1 != s2 && s2 == cpu.SF) ;
+    	cpu.ZF=!result;
+    	OPERAND_W(op_dest, result);
+	result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF=!(result & 1);
+	print_asm_no_template2();
 }
+
 
 make_instr_helper(i2rm)
 
