285b8650a4407d83bd5077ab01870efc9b4f23ed
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-25 21:38:38 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:38:38 up 23:10,  1 user,  load average: 1.56, 1.27, 1.12 9fa7eafbb3d9495763e97947d184408ff1b10e

--- nemu/src/cpu/exec/data-control/jbe-template.h
+++ nemu/src/cpu/exec/data-control/jbe-template.h
@@ -3,9 +3,9 @@
 #define instr jbe
 
 static void do_execute() {
-	DATA_TYPE_S displacement = op_src->val;
-	print_asm("jbe %x",cpu.eip + 1 + DATA_BYTE + displacement);
-	if (cpu.CF == 1 || cpu.ZF == 1)cpu.eip +=displacement;
+    DATA_TYPE_S imm = op_src->val;
+    print_asm("jbe %x",cpu.eip + DATA_BYTE+1 + imm);
+    if(cpu.CF == 1 || cpu.ZF == 1) cpu.eip += imm;
 }
 
 make_instr_helper(i)
