87c90b1a6a81be8505678b25f5ebc1806030d039
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-26 15:19:14 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:19:14 up  5:03,  1 user,  load average: 0.62, 0.82, 0.96 c6e9c16596a23c7c82901ceb6dfabb29f754933b

---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,41 @@
+#include "cpu/exec/template-start.h"
+
+#define instr adc
+
+static void do_execute() {//和sub的基本相同，只需修改-成+，然后改一下flag的影响
+	DATA_TYPE result = op_dest->val + op_src->val + cpu.CF;
+	OPERAND_W(op_dest, result);
+	//检查无符号数溢出
+	cpu.CF = (result < op_dest->val);
+	// = 结果的符号位
+    cpu.SF = result >> (DATA_BYTE*8 -1);
+    //检查有符号数溢出
+    int a = op_dest->val >> (DATA_BYTE*8 - 1);
+    int b = op_src->val >> (DATA_BYTE*8 - 1);
+		// a + b + c = result
+		// +   +        -
+		// -   -        +
+		//因此，当a和b符号相同，结果和b的符号不同时，有符号数溢出
+    cpu.OF = (a == b) && (b != cpu.SF);
+    // 结果==0 为1
+    cpu.ZF = !result;
+	// 结果的最低有效字节包含偶数个1 为1
+    result ^= result >>4;
+	result ^= result >>2;
+	result ^= result >>1;
+	cpu.PF = !(result & 1);
+    //D3到D4是否进位
+    if(( (op_dest->val & 0xf) + (op_src->val & 0xf)  + cpu.CF) >> 4) cpu.AF = 1;
+    else cpu.AF = 0;
+	print_asm_template2();
+}
+
+make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+
+#include "cpu/exec/template-end.h"
