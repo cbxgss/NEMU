75070269b3aac5ac1fc13e1165cb79c38ec37aa1
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 18:29:14 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:29:14 up 21 min,  1 user,  load average: 0.67, 0.67, 0.43 db5f5c22388a952af3c910522dae3b6336eac5c

--- nemu/src/cpu/exec/data-control/je-template.h
+++ nemu/src/cpu/exec/data-control/je-template.h
@@ -4,7 +4,7 @@
 
 static void do_execute() {
 	DATA_TYPE_S imm = op_src->val;
-    printf("%d\t", imm);
+    printf("eip:%d\tDATA_BYTE:%d\timm:%d\t", cpu.eip, DATA_BYTE, imm);
     print_asm("je %x",cpu.eip + DATA_BYTE+1 + imm);
     if(cpu.ZF == 1) cpu.eip += imm;
     // print_asm("je %x",cpu.eip + DATA_BYTE+1);   //注意上面已经+imm了     这是原来的写法，实际上不对，因为不管怎样，输出指令的时候都应该加上imm
