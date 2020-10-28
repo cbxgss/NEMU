e619aabb0def3deee5e4e0e0371d426cb90401e5
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 18:50:43 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:50:43 up 42 min,  1 user,  load average: 0.90, 0.67, 0.63 f0bbb145209e6fa8ad250ad784cf80ec309ef46

--- nemu/src/cpu/exec/data-control/je-template.h
+++ nemu/src/cpu/exec/data-control/je-template.h
@@ -5,6 +5,7 @@
 static void do_execute() {
 	DATA_TYPE_S imm = op_src->val;
     // printf("eip:%x\tDATA_BYTE:%x\timm:%x\t", cpu.eip, DATA_BYTE, imm);
+    // 因为上面修改了exec.c文件里的0f开头的函数，应该先cpu.eip++，不然这里je输出的时候总是少1
     print_asm("je %x",cpu.eip + DATA_BYTE+1 + imm);
     if(cpu.ZF == 1) cpu.eip += imm;
     // print_asm("je %x",cpu.eip + DATA_BYTE+1);   //注意上面已经+imm了     这是原来的写法，实际上不对，因为不管怎样，输出指令的时候都应该加上imm
