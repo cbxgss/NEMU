778013b8ffe6ce83273e0ce44819e328808b776f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 19:10:24 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:10:24 up  1:02,  1 user,  load average: 0.70, 0.65, 0.63 58443ffa24e2cce532775069849ec21878c580f1

--- nemu/src/cpu/exec/data-control/pop-template.h
+++ nemu/src/cpu/exec/data-control/pop-template.h
@@ -2,11 +2,22 @@
 
 #define instr pop
 
-static void do_execute() {
-    swaddr_write(op_src->addr, 4, MEM_R(reg_l(R_SP)));
-    reg_l(R_SP) += 4;
-    print_asm_no_template1();
+static void do_execute () {
+	if (DATA_BYTE == 1)
+	{
+		swaddr_write (op_src->addr, 4 ,MEM_R(reg_l (R_ESP)));
+		MEM_W(reg_l (R_ESP) , 0);
+		reg_l (R_ESP) += 4;
+	}
+	else
+	{
+		OPERAND_W (op_src, MEM_R(REG (R_ESP)));
+		MEM_W(REG (R_ESP) , 0);
+		REG (R_ESP) += DATA_BYTE;
+	}
+	print_asm_no_template1();
 }
+
 
 make_instr_helper(r)
 
--- nemu/src/cpu/exec/data-control/push-template.h
+++ nemu/src/cpu/exec/data-control/push-template.h
@@ -2,10 +2,11 @@
 
 #define instr push
 
-static void do_execute() {
-        reg_l(R_SP) -= 4;                       //为了pop方便，统一-4
-        swaddr_write(reg_l(R_SP), 4, (DATA_TYPE_S)op_src->val);
-        print_asm_template1();
+static void do_execute () {
+	if (DATA_BYTE == 1)op_src->val = (int8_t)op_src->val;
+	reg_l (R_ESP) -= 4;
+	swaddr_write (reg_l (R_ESP) , 4 , op_src->val);
+	print_asm_no_template1();
 }
 
 make_instr_helper(r)
