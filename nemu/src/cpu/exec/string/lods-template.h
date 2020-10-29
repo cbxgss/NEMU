02517441625fb26ef50e7df5d879253e31207caf
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 22:18:48 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:18:48 up 12:56,  1 user,  load average: 0.83, 0.77, 0.86 311e7aa037affd8f7b24b2e4a6e521e027eabbb9

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -5,8 +5,8 @@
 make_helper(concat(lods_n_, SUFFIX)){//ac; 和movs类似
 	// 内存加载。如果操作数为1字节，则将其装载到AL寄存器中，如果操作数为一个字，则它将被加载到AX寄存器中，并将一个双字加载到EAX寄存器中。 
 	swaddr_write (reg_b(R_AL), 1, swaddr_read (reg_l(R_ESI), 1));
-	if (cpu.DF == 0) { reg_b (R_AL) += 1; reg_l (R_ESI) += 1; }
-	else { reg_b (R_AL) -= 1; reg_l (R_ESI) -= 1; }
+	if (cpu.DF == 0) { reg_b (R_AL) += DATA_BYTE; reg_l (R_ESI) += DATA_BYTE; }
+	else { reg_b (R_AL) -= DATA_BYTE; reg_l (R_ESI) -= DATA_BYTE; }
 	print_asm("lods");
     return 1;
 }
--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -4,10 +4,38 @@
 
 make_helper(concat(scas_n_, SUFFIX)){//ae ; 和lods类似
 	// 将寄存器（AL，AX或EAX）的内容与内存中的项目的内容进行比较
-	swaddr_write (reg_b(R_AL), 1, swaddr_read(reg_b(R_AL), 1) - swaddr_read (reg_l(R_EDI),1));
-	if (cpu.DF == 0) { reg_b (R_AL) += 1; reg_l (R_EDI) += 1; }
-	else { reg_b (R_AL) -= 1; reg_l (R_EDI) -= 1; }
+	DATA_TYPE dest = swaddr_read(reg_b(R_AL), 1); DATA_TYPE src = swaddr_read (reg_l(R_EDI),1);
+	DATA_TYPE result = dest - src;
+	swaddr_write (reg_b(R_AL), 1, result);
+	if (cpu.DF == 0) { reg_b (R_AL) += DATA_BYTE; reg_l (R_EDI) += DATA_BYTE; }
+	else { reg_b (R_AL) -= DATA_BYTE; reg_l (R_EDI) -= DATA_BYTE; }
 	print_asm("scas");
+
+	//检查无符号数溢出
+	if(dest < src) cpu.CF = 1;
+    else cpu.CF = 0;
+	// = 结果的符号位
+    cpu.SF = result >> (DATA_BYTE*8 - 1);
+    //检查有符号数溢出
+    int a = op_dest->val >> (DATA_BYTE*8 - 1);
+    int b = op_src->val >> (DATA_BYTE*8 - 1);
+		// a - b  = result
+		// +   -        -
+		// -   +        +
+		//因此，当a和b符号不同，结果和b的符号相同时，有符号数溢出
+    cpu.OF = (a != b) && (b == cpu.SF);
+    // 结果==0 为1
+    cpu.ZF = !result;
+	// 结果的最低有效字节包含偶数个1 为1
+    int qwq = 0;
+    int i;
+    for(i = 0; i < 8; i++) qwq += (result >> i) & 1;
+    if(qwq % 2) cpu.PF = 0;
+    else cpu.PF = 1;
+    //D3到D4是否进位
+    if(( (op_dest->val & 0xf) - (op_src->val & 0xf) ) >> 4) cpu.AF = 1;
+    else cpu.AF = 0;
+
     return 1;
 }
 
