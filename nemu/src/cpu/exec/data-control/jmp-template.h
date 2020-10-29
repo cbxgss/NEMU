7d42e75338c1096514781db7e6bc2dc4f6079820
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 16:46:40 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:46:40 up  7:23,  1 user,  load average: 1.00, 1.02, 0.99 6167b00b6f158a79cf581bcba039fb157fbb44

--- nemu/src/cpu/exec/data-control/jmp-template.h
+++ nemu/src/cpu/exec/data-control/jmp-template.h
@@ -9,9 +9,9 @@ static void do_execute() {
         print_asm("jmp %x",cpu.eip + DATA_BYTE+1);
     }
     else {
-        printf("imm:%x\t", imm);
-        // 似乎imm直接就是要
-        cpu.eip = imm - concat (decode_rm_,SUFFIX)(cpu.eip+1) - 1;
+        // printf("imm:%x\t", imm);
+        // 通过上面的printf可以推断，似乎imm直接就是要Jum的地址，所以下面要减去该指令的长度
+        cpu.eip = imm - (concat (decode_rm_,SUFFIX)(cpu.eip+1) + 1);
 		print_asm_no_template1();
     }
 }
