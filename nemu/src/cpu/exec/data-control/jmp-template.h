2deefb9c391977ce2ec62dd94a34d44d014fa423
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 16:39:46 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:39:46 up  7:17,  1 user,  load average: 1.69, 1.16, 0.99 4f463e6d18dcfa86906c8b589c6979adb639f58b

--- nemu/src/cpu/exec/data-control/jmp-template.h
+++ nemu/src/cpu/exec/data-control/jmp-template.h
@@ -4,13 +4,14 @@
 
 static void do_execute() {
     DATA_TYPE_S imm = op_src->val;
-    // if( op_src->type == OP_TYPE_IMM ){
+    if( op_src->type == OP_TYPE_IMM ){
         cpu.eip += imm;
         print_asm("jmp %x",cpu.eip + DATA_BYTE+1);
-    // }
-    // else {
-
-    // }
+    }
+    else {
+        cpu.eip = imm - concat (decode_rm_,SUFFIX)(cpu.eip+1) - 1;
+		print_asm_no_template1();
+    }
 }
 
 make_instr_helper(i)
