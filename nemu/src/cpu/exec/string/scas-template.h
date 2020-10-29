73c88aee5374a83807353c97785be4d788171468
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 21:42:44 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:42:44 up 12:20,  1 user,  load average: 0.96, 0.91, 0.87 c6442819502ff8ecb96cb7f646400c4c1a2acb51

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -3,6 +3,7 @@
 #define instr scas
 
 make_helper(concat(scas_n_, SUFFIX)){//ae ; 和lods类似, 改几个字母就ok
+	// 将寄存器（AL，AX或EAX）的内容与内存中的项目的内容进行比较
     if ( ops_decoded.is_operand_size_16 ) {
 		swaddr_write (reg_w(R_AL), 2, swaddr_read (reg_w(R_AL),4) - swaddr_read (reg_w(R_DI),4));
 		if (cpu.DF == 0) { reg_w (R_AL) += DATA_BYTE; reg_w (R_DI) += DATA_BYTE; }
