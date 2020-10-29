134a695fe2b00b205c027ebbf58304a38d1e9ff3
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 19:05:54 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:05:54 up  9:43,  1 user,  load average: 0.88, 0.81, 0.83 cc93f9f4d8bce13fefdf306edd9e071e831e3c8

--- nemu/src/cpu/exec/data-control/ret-template.h
+++ nemu/src/cpu/exec/data-control/ret-template.h
@@ -3,11 +3,28 @@
 #define instr ret
 
 make_helper(concat(ret_n_, SUFFIX)){//没有相应的decode，所以自己写
+    //ret
     cpu.eip = MEM_R(reg_l(R_SP));
     if(DATA_BYTE == 2) cpu.eip &= 0xffff;
     reg_l(R_SP) += DATA_BYTE;
+
     print_asm("ret");
     return 1;
+}
+
+make_helper(concat(ret_i_, SUFFIX)){//c2
+    //ret
+	cpu.eip = MEM_R (REG (R_ESP));
+	if (DATA_BYTE == 2) cpu.eip &= 0xffff;
+	REG (R_ESP) += DATA_BYTE;
+    //pop immm个字节
+    int val = instr_fetch(eip + 1, 2);
+	int i;
+	for (i = 0; i < val; i += DATA_BYTE) MEM_W (REG (R_ESP) + i,0);//初始化成0
+	REG (R_ESP) += val;
+
+	print_asm("ret $0x%x",val);
+	return 1;   //尽管实际长度是3字节，但是有关eip的赋值，还是应该返回1
 }
 
 #include "cpu/exec/template-end.h"
