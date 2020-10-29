fc45dc2ec3993195ef407abd42319d1c5db539ad
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 22:07:28 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:07:28 up 12:44,  1 user,  load average: 0.70, 0.88, 0.95 fb3e2b60124b3f557d64d50b13d1fd83770d05be

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -4,9 +4,9 @@
 
 make_helper(concat(scas_n_, SUFFIX)){//ae ; 和lods类似
 	// 将寄存器（AL，AX或EAX）的内容与内存中的项目的内容进行比较
-	swaddr_write (reg_b(R_AL), 1, swaddr_read(reg_b(R_AL), 1) - swaddr_read (reg_w(R_SI),1));
-	if (cpu.DF == 0) { reg_b (R_AL) += DATA_BYTE; reg_w (R_SI) += DATA_BYTE; }
-	else { reg_b (R_AL) -= DATA_BYTE; reg_w (R_SI) -= DATA_BYTE; }
+	swaddr_write (reg_b(R_AL), 1, swaddr_read(reg_b(R_AL), 1) - swaddr_read (reg_l(R_EDI),1));
+	if (cpu.DF == 0) { reg_b (R_AL) += DATA_BYTE; reg_l (R_EDI) += DATA_BYTE; }
+	else { reg_b (R_AL) -= DATA_BYTE; reg_l (R_EDI) -= DATA_BYTE; }
 	print_asm("scas");
     return 1;
 }
