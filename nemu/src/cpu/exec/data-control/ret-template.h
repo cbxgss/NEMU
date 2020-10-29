61c235b0f3e2e12c391860623e5e6d00a9ce9914
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-29 18:45:33 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:45:33 up  9:22,  1 user,  load average: 1.78, 1.24, 1.08 3b6a3cbc395cd97d44fee5eb23585710f4aa6cce

--- nemu/src/cpu/exec/data-control/ret-template.h
+++ nemu/src/cpu/exec/data-control/ret-template.h
@@ -2,7 +2,7 @@
 
 #define instr ret
 
-make_helper(concat(ret_n_, SUFFIX)){
+make_helper(concat(ret_n_, SUFFIX)){//没有相应的decode，所以自己写
     cpu.eip = MEM_R(reg_l(R_SP));
     if(DATA_BYTE == 2) cpu.eip &= 0xffff;
     reg_l(R_SP) += DATA_BYTE;
