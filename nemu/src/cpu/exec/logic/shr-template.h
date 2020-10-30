a37c6052b96a137259fe8f4de233356324197c27
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 10:49:56 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:49:56 up 1 day,  1:27,  1 user,  load average: 0.98, 0.91, 0.80 8ab71f976358c7c512c12b9ecc9ece1c95ac380

--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -11,6 +11,19 @@ static void do_execute () {
 	OPERAND_W(op_dest, dest);
 
 	/* TODO: Update EFLAGS. */
+	//检查无符号数溢出
+    cpu.CF = 0;
+    // = 结果的符号位
+    cpu.SF = dest >> (DATA_BYTE*8 -1);
+    //检查有符号数溢出
+    cpu.OF = 0;
+    // 结果==0 为1
+    cpu.ZF = !dest;
+    // 结果的最低有效字节包含偶数个1 为1
+    dest ^= dest >>4;
+	dest ^= dest >>2;
+	dest ^= dest >>1;
+	cpu.PF = !(dest & 1);
 	// panic("please implement me");
 
 	print_asm_template2();
