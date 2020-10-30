6abaffb0921b846dca1f1f4e69058433b3e20b17
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-30 10:15:56 +0800

    >  run 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:15:56 up 1 day, 53 min,  1 user,  load average: 1.15, 1.26, 1.10 efeeea48ea7335b3b5b63eb6236f269251316c9b

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -23,7 +23,7 @@ int main() {
 	// nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 	// memset(str, '#', 4);
 	// nemu_assert(strncmp(str, s[5], 5) == 0);
-	nemu_assert(memcmp(memset(str, '#', 5), s[5], 1) == 0);
+	nemu_assert(memcmp(memset(str, '#', 5), s[5], 2) == 0);
 
 	return 0;
 }
