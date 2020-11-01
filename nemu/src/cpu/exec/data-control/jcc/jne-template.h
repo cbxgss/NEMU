50fb2591509c498017f811c538cf5d927aa3445d
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-01 21:32:33 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:32:33 up 3 days, 12:09,  1 user,  load average: 1.10, 1.16, 1.10 7a5159fc2b596daad1c37971211998b583fba06d

--- nemu/src/cpu/exec/data-control/jcc/jne-template.h
+++ nemu/src/cpu/exec/data-control/jcc/jne-template.h
@@ -2,13 +2,11 @@
 
 #define instr jne
 
-
 static void do_execute() {
 	DATA_TYPE_S imm = op_src->val;
 	print_asm("jne %x",cpu.eip + 1 + DATA_BYTE + imm);
 	if (cpu.ZF == 0) cpu.eip += imm;
 }
 make_instr_helper(i)
-
 
 #include "cpu/exec/template-end.h"
