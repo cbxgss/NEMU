a49c1e92771b4c16b861a7a60dd0868fb14c6a5f
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-01 21:46:57 +0800

    >  run 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:46:57 up 3 days, 12:24,  1 user,  load average: 1.02, 1.40, 1.35 8c8e18f7ce36eafa3dd3cd37bbf0e7173a3d7f

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -9,67 +9,55 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	int sign = 1;
-	if (a < 0) 
-	{
-		sign = -sign;
-		a = -a;
-	}
-	if (b < 0) 
-	{
-		sign = -sign;
-		b = -b;
-	}
-	int res = a / b;
+	/* Dividing two 64-bit integers needs the support of another library
+	 * `libgcc', other than newlib. It is a dirty work to port `libgcc'
+	 * to NEMU. In fact, it is unnecessary to perform a "64/64" division
+	 * here. A "64/32" division is enough.
+	 *
+	 * To perform a "64/32" division, you can use the x86 instruction
+	 * `div' or `idiv' by inline assembly. We provide a template for you
+	 * to prevent you from uncessary details.
+	 *
+	 *     asm volatile ("??? %2" : "=a"(???), "=d"(???) : "r"(???), "a"(???), "d"(???));
+	 *
+	 * If you want to use the template above, you should fill the "???"
+	 * correctly. For more information, please read the i386 manual for
+	 * division instructions, and search the Internet about "inline assembly".
+	 * It is OK not to use the template above, but you should figure
+	 * out another way to perform the division.
+	 */
+
+	// nemu_assert(0);
+	// 符号(直接在整数部分处理的话，可能有1的误差)：
+	int qwq = 1;
+	if(a < 0) { qwq *= -1; a = -a; }
+	if(b < 0) { qwq *= -1; b = -b; }
+	// 整数部分：a/b
+	FLOAT x = (a / b);
+	// 小数部分：
+	// for 每一位小数：		x = x * 10 + 1 ? 0 : 还有
 	a = a % b;
-	int i;
-	for (i = 0; i < 16; i++) 
-	{
-		a <<= 1;
-		res <<= 1;
-		if (a >= b) 
-		{
-			a -= b;
-			res++;
+	int i = 0;
+	for(i; i < 16; i++){
+		a <<= 1; x <<= 1;
+		if(a >= b){	//（a<<i / b） >= 1时
+			a -= b; x++;
 		}
 	}
-	return res * sign;
+	return x * qwq;
 }
 
 FLOAT f2F(float a) {
-	/* You should figure out how to convert `a' into FLOAT without
-	 * introducing x87 floating point instructions. Else you can
-	 * not run this code in NEMU before implementing x87 floating
-	 * point instructions, which is contrary to our expectation.
-	 *
-	 * Hint: The bit representation of `a' is already on the
-	 * stack. How do you retrieve it to another variable without
-	 * performing arithmetic operations on it directly?
-	 */
-	//   提示：“ a”的位表示形式已经在堆栈中。 如何在不直接对其执行算术运算的情况下将其检索到另一个变量？
-
-	// nemu_assert(0);
-	// float : 1 sign + 8 exp + 23 frac
-	// 值：	规格化：					(-1)^s * 2^(exp - 127) * (1.frac) = (-1)^s * 2^(exp - 150) * (1frac)
-	// 	   非规格化：	exp == 0		(-1)^s * 2^(1-127) * 0.frac = (-1)^s * 2^(1-150) * frac
-	// 读取float a
-	int b = *(int *)(&a);		//(int)a会真的变成int，所以用指针
+	int b = *(int *)&a;
 	int sign = b >> 31;
 	int exp = (b >> 23) & 0xff;
-	FLOAT frac = b & 0x7fffff;
-	// 规格化和非规格化，全部指数化
-	if (exp != 0) frac += 1 << 23;		//如果是规格化的，小数点前有1
-	else exp = 1;						//如果非规格话，那么exp当1处理
+	FLOAT k = b & 0x7fffff;
+	if (exp != 0) k += 1 << 23;
 	exp -= 150;
-	// 得到FLOAT
-	// frac << exp << 16		即		frac << (exp + 16)
-	if(exp + 16 > 0) frac <<= exp + 16;
-	else if(exp + 16 < 0) frac >>= -(exp + 16);
-	// 返回，加上符号
-	if(sign) return frac;
-	else return -frac;
+	if (exp < -16) k >>= -16 - exp;
+	if (exp > -16) k <<= exp + 16;
+	return sign == 0 ? k : -k;
 }
-
 FLOAT Fabs(FLOAT a) {	//返回浮点数的绝对值
 	// nemu_assert(0);
 	if(a < 0) return -a;
