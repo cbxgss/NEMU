8c9f5afe96d900bbd2557ea7718bd2d6d51a76cc
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-01 23:04:51 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:04:51 up 3 days, 13:41,  1 user,  load average: 1.62, 1.35, 1.24 2c075d44c8f74136be21fb32be8af919cf88bec6

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -4,14 +4,21 @@
 
 static void do_execute () {
 	if (ops_decoded.is_operand_size_16) {
-        swaddr_write (reg_w(R_DI), 2, reg_w(R_AX));
+        swaddr_write (reg_l(R_EDI), 2, reg_l(R_EAX));
         if (cpu.DF == 0) reg_w (R_DI) += DATA_BYTE;
         else reg_w (R_DI) -= DATA_BYTE;
     }
     else {
-        swaddr_write (reg_l(R_EDI), 4, reg_l(R_EAX));
-        if (cpu.DF == 0) reg_l (R_EDI) += DATA_BYTE;
-        else reg_l (R_EDI) -= DATA_BYTE;
+        if(DATA_BYTE == 4) {
+            swaddr_write (reg_l(R_EDI), 4, reg_l(R_EAX));
+            if (cpu.DF == 0) reg_l (R_EDI) += DATA_BYTE;
+            else reg_l (R_EDI) -= DATA_BYTE;
+        }
+        if(DATA_BYTE == 1) {
+            swaddr_write (reg_l(R_EDI), 1, reg_l(R_EAX));
+            if (cpu.DF == 0) reg_l (R_EDI) += DATA_BYTE;
+            else reg_l (R_EDI) -= DATA_BYTE;
+        }
     }
     print_asm("stos");
 }
