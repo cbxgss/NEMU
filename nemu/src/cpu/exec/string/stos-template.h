b7c2b852eb8d4e7bf11e09524694d45b44e0e9f2
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-01 20:03:14 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:03:14 up 3 days, 10:40,  1 user,  load average: 1.28, 1.33, 1.33 25f4e86cdc5cac61d9e74d57c3ca5d0d70dcdd07

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -2,11 +2,8 @@
 
 #define instr stos
 
-make_helper(concat(stos_n_, SUFFIX)) {
-    // swaddr_write (reg_w(R_DI), 1, reg_b(R_AL));
-    // if (cpu.DF == 0) reg_w (R_DI) += DATA_BYTE;
-    // else reg_w (R_DI) -= DATA_BYTE;
-    if (ops_decoded.is_operand_size_16) {
+static void do_execute () {
+	if (ops_decoded.is_operand_size_16) {
         swaddr_write (reg_w(R_DI), 2, reg_w(R_AX));
         if (cpu.DF == 0) reg_w (R_DI) += DATA_BYTE;
         else reg_w (R_DI) -= DATA_BYTE;
@@ -17,8 +14,8 @@ make_helper(concat(stos_n_, SUFFIX)) {
         else reg_l (R_EDI) -= DATA_BYTE;
     }
     print_asm("stos");
-    return 1;
 }
 
+make_instr_helper(n)
 
 #include "cpu/exec/template-end.h"
