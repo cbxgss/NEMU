76e3670b257e3af0bb7d32f1a8c21f04192c641a
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-02 17:26:17 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:26:17 up 4 days,  8:03,  1 user,  load average: 1.77, 1.19, 1.14 86f79af232bde4331bc776877315d87293929404

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -277,7 +277,7 @@ int eval(int p, int q) {
 					int tmplen = symtab[i+1].st_name - symtab[i].st_name - 1;
 					strncpy (tmp, strtab + symtab[i].st_name, tmplen);
 					// printf("%s :", tmp);
-					tmp [tmplen] = 0;
+					tmp [tmplen] = '\0';
 					printf("%s :", tmp);
 					// if (strcmp (tmp, tokens[p].str) == 0) return symtab[i].st_value;
 					return symtab[i+1].st_name - symtab[i].st_name - 1;
