e629c772aa71a9dfccc360272e7252e64ecabc94
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-02 21:10:22 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:10:22 up 4 days, 11:47,  1 user,  load average: 1.54, 1.32, 1.22 f3fb6494b78135596cf0cfd52b740338612adc4b

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -9,16 +9,9 @@ static void do_execute () {
         else reg_w (R_DI) -= DATA_BYTE;
     }
     else {
-        if(DATA_BYTE == 4) {
-            swaddr_write (reg_l(R_EDI), 4, reg_l(R_EAX));
-            if (cpu.DF == 0) reg_l (R_EDI) += DATA_BYTE;
-            else reg_l (R_EDI) -= DATA_BYTE;
-        }
-        if(DATA_BYTE == 1) {
-            swaddr_write (reg_l(R_EDI), 1, reg_l(R_EAX));
-            if (cpu.DF == 0) reg_l (R_EDI) += DATA_BYTE;
-            else reg_l (R_EDI) -= DATA_BYTE;
-        }
+        swaddr_write (reg_l(R_EDI), DATA_BYTE, reg_l(R_EAX));
+        if (cpu.DF == 0) reg_l (R_EDI) += DATA_BYTE;
+        else reg_l (R_EDI) -= DATA_BYTE;
     }
     print_asm("stos");
 }
