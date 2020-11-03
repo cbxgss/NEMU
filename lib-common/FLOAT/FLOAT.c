cc369544f18898c7f88be6b9b9216ce13ba9a7ca
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-03 16:19:59 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:19:59 up 5 days,  6:56,  1 user,  load average: 2.15, 1.75, 1.56 ca406c9abf9a05f58c155b7af2627d9d2ad03f0

--- lib-common/FLOAT/FLOAT.c
+++ lib-common/FLOAT/FLOAT.c
@@ -1,4 +1,6 @@
 #include "FLOAT.h"
+#include <string.h>
+#include <stdlib.h>
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
 	// nemu_assert(0);
@@ -64,10 +66,14 @@ FLOAT f2F(float a) {
 	// 值：	规格化：					(-1)^s * 2^(exp - 127) * (1.frac) = (-1)^s * 2^(exp - 150) * (1frac)
 	// 	   非规格化：	exp == 0		(-1)^s * 2^(1-127) * 0.frac = (-1)^s * 2^(1-150) * frac
 	// 读取float a
-	int b = *(int *)&a;		//(int)a会真的变成int，所以用指针
+	// int b = *(int *)&a;		//(int)a会真的变成int，所以用指针
+	int bb[1];
+	memcpy((void *)bb, (void *)&a, 4);
+	int b = bb[1];
 	int sign = b >> 31;
 	int exp = (b >> 23) & 0xff;
 	FLOAT frac = b & 0x7fffff;
+	free(bb);
 	// 规格化和非规格化，全部指数化
 	if (exp != 0) frac += 1 << 23;		//如果是规格化的，小数点前有1
 	else exp = 1;						//如果非规格话，那么exp当1处理
