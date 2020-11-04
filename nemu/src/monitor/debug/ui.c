25528bbb9e59fe7ed732b09d9150c6e36f5fa0b0
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-04 19:30:03 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:30:03 up  6:01,  1 user,  load average: 0.47, 0.84, 0.93 d2d45e465381bff1cbb836f2aa601e1a55d6db6a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -124,7 +124,6 @@ static int cmd_bt(char *args) {
 				if(symtab[j].st_value <= now.ret_addr && now.ret_addr < symtab[j].st_value + symtab[j].st_size) {//在里面
 					//函数的名字
 					char f_name[32];	strcpy(f_name, strtab + symtab[j].st_name);
-					printf("%s\n", strtab + symtab[i].st_name);
 					//打印
 					printf("#%d\t0x%08x in %s (%d, %d, %d, %d)\n", i++, now.ret_addr, f_name, now.args[0], now.args[1], now.args[2], now.args[3]);
 					//更新ebp和now
