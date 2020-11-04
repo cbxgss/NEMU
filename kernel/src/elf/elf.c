66c60fc286cff3f2978c4a089b4e96fe80b9fc20
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-04 23:03:14 +0800

    >  compile kernel 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:03:14 up  9:34,  1 user,  load average: 1.54, 1.20, 1.21 c99d955d2986906b4d958936cec7d19e4b2b608

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -2,7 +2,7 @@
 #include "memory.h"
 #include <string.h>
 #include <elf.h>
-#include <stdio.h>
+
 #define ELF_OFFSET_IN_DISK 0
 
 #ifdef HAS_DEVICE
@@ -19,44 +19,46 @@ uint32_t get_ucr3();
 uint32_t loader() {
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
+
 	uint8_t buf[4096];
 
 #ifdef HAS_DEVICE
 	ide_read(buf, ELF_OFFSET_IN_DISK, 4096);
 #else
-	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);
+	ramdisk_read(buf, ELF_OFFSET_IN_DISK, 4096);	//从 ramdisk 中 offset 偏移处的 len 字节读入到 buf 中
 #endif
+
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	const uint32_t elf_magic = 0x464c457f;
+	const uint32_t elf_magic = 0x464c457f;					//readelf开头有magic，开头4个字节
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
+
 	/* Load each program segment */
-	//panic("please implement me");
-	int i = 0;
-	ph = (void *)(buf+elf->e_phoff) ;
-	for(; i < elf->e_phnum ; ++i,++ph) {
+	panic("please implement me");
+	// int i = 0;
+	// ph = (void *)(buf + elf->e_phoff) ;
+	// for(i = 0; i < elf->e_phnum ; i++, ph++) {
+	for(; true ;) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
-			ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
-			 */	
-#ifdef HAS_DEVICE
-			ide_read((void *)ph->p_vaddr,ph->p_offset,ph->p_filesz);
-#else	
-			ramdisk_read ((void *)ph->p_vaddr,ph->p_offset,ph->p_filesz);
-#endif				 
+			 */
+			// ramdisk_read ((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
+
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			 memset ((void *)(ph->p_vaddr+ph->p_filesz),0,ph->p_memsz-ph->p_filesz);
+			// memset ((void *)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
-			extern uint32_t brk;
+			extern uint32_t cur_brk, max_brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if(brk < new_brk) { brk = new_brk; }
+			if(cur_brk < new_brk) { max_brk = cur_brk = new_brk; }
 #endif
 		}
 	}
