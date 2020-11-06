8e539d1d9c92fbfce9eef61e79acc05369600f75
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-06 23:24:28 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:24:28 up 1 day, 13:33,  1 user,  load average: 1.40, 1.48, 1.42 18e7eea6b9626520751e34c2bcbf3c6c3b0a6c0f

--- nemu/src/cpu/exec/logic/sete.c
+++ nemu/src/cpu/exec/logic/sete.c
@@ -4,5 +4,14 @@
 #include "sete-template.h"
 #undef DATA_BYTE
 
+#define DATA_BYTE 2
+#include "sete-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "sete-template.h"
+#undef DATA_BYTE
+
+
 /* for instruction encoding overloading */
 
