0ce0e588d355eab8c4edbd68c3d4c4cb01c3d597
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-06 23:05:07 +0800

    >  run 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:05:07 up 1 day, 13:14,  1 user,  load average: 1.51, 1.45, 1.30 36278414423efe72aab27e2636fe78dc4829bd1

--- testcase/src/print-FLOAT.c
+++ testcase/src/print-FLOAT.c
@@ -22,7 +22,7 @@ int main() {
 
 	sprintf(buf, "%f %d", FLOAT_ARG(0xfffecccd), 123456);
 	// nemu_assert(strcmp(buf, "-1.199996 123456") == 0);
-	nemu_assert(strcmp(buf, "-1.199996 123456") == 0);
+	nemu_assert(strncmp(buf, "-1.199996 123456", 17) == 0);
 #endif
 
 	return 0;
