6a27b0d0b9a4dd7871d46a489a466ad76f581bc6
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-01 11:12:26 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:12:26 up  2:19,  1 user,  load average: 1.38, 1.15, 0.95 527114ced6f4a4f2a3b8c460a2d564836744310

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,12 +1,14 @@
 #include "common.h"
+#include <stdlib.h>
+#include "burst.h"
 
 #define block_bytes 64		// 一个块的字节
-#define Cache_ways 8			// 几路组相连
+#define Cache_ways 8		// 几路组相连
 #define Cache_sets 1024/8	// Cache的组数
 
 /* Cache的class */
 typedef struct block {
-	uint32_t tag;					// 标记位
+	uint32_t tag;					// 标记位(19位)
 	uint8_t block[block_bytes];		// 64个字节
 	bool valid;					// valid bit
 } block;
@@ -16,12 +18,14 @@ typedef struct set {
 typedef struct Cache {
 	/* 成员属性 */
 	set sets[Cache_sets];			// 2**10个块
+	uint64_t t_sum;					// 计时变量
 	/* 成员函数 */
 	// void (* init)(struct Cache *this);
 } Cache;
-Cache cache;
+Cache cache;		// 实例化
 /* 成员函数的实现 */
 void init_cache() {
+	cache.t_sum = 0;
 	int i, j;
 	for(i = 0; i < Cache_sets; i++) {
 		for(j = 0; j < Cache_ways; j++) {
@@ -34,6 +38,39 @@ uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
 /* Memory accessing interfaces */
+
+uint32_t cache_read(hwaddr_t addr) {			//返回是set_index的哪个block
+	// 地址32位 = 19位tags + 7位sets + 6位块内偏移
+	uint32_t tag_now = (addr >> 13) & 0x7ffff;
+	uint32_t set_now = (addr >> 6) & 0x7f;
+	// uint32_t imm_now = addr & 0x3f;
+	bool hit = false;
+	int i;
+	for (i = 0; i < Cache_ways; i++) {	//在set中每个block检查
+		if ( !cache.sets[set_now].blocks[i].valid ) continue;
+		if ( cache.sets[set_now].blocks[i].tag == tag_now ) {
+			hit = true; break;
+		}
+	}
+	if(!hit) {	// miss
+		// 找到位置
+		for (i = 0; i < Cache_ways; i++) {
+			if ( !cache.sets[set_now].blocks[i].valid ) break;
+		}
+		if(i == Cache_ways) {
+			srand(0);
+			i = rand() % Cache_ways;
+		}
+		// 复制到这个块
+		cache.sets[set_now].blocks[i].valid = true;
+		cache.sets[set_now].blocks[i].tag = tag_now;
+		// int j;
+		// for ( j = 0; j < BURST_LEN; j++ )
+			// ddr3_read(((addr>>6)<<6) + j * BURST_LEN, &cache.sets[set_now].blocks[i] + j*BURST_LEN);
+
+	}
+	return i;
+}
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
