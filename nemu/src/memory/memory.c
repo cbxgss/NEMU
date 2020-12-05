46b91bba9c8455fc3de407da478cbd03cf8b8828
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-05 10:15:06 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:15:06 up 42 min,  1 user,  load average: 0.57, 0.73, 0.66 f9cabc4da91317c4fe3b286d5317f3eac300eb0a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -80,7 +80,7 @@ uint32_t cache_read(hwaddr_t addr) {
 
 // 读从addr开始的len个字节
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-	printf("(0x%x) ", addr);
+	// printf("(0x%x) ", addr);
 	/* 原来的代码 */
 	// return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 
