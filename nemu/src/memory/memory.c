cb76116f0743f325c077eebd43335ecceaf32c1e
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-05 21:15:20 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:15:20 up  3:34,  1 user,  load average: 1.61, 1.25, 1.04 27a4a8e1604f621af3bd5513fe1bf2a848cdc0d

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -93,7 +93,7 @@ uint32_t cache_read(hwaddr_t addr) {
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	// printf("(0x%x) ", addr);
 	/* 原来的代码 */
-	return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+	// return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 
 	/* 加上 chahe 之后的代码 */
 	// 地址处理
@@ -119,7 +119,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	/* 原来的代码 */
-	dram_write(addr, len, data);
+	// dram_write(addr, len, data);
 	/* 加入cache后的代码 */
 	uint32_t tag_now = (addr >> 13) & 0x7ffff;
 	uint32_t set_now = (addr >> 6) & 0x7f;
