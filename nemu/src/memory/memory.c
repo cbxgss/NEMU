ab4b9b9d905e740e2ce2876bd2f16e15a34192bc
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-05 21:28:35 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.4.0-42-generic #46-Ubuntu SMP Fri Jul 10 00:24:02 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:28:35 up  3:47,  1 user,  load average: 0.67, 0.73, 0.83 86206c28faad67b561b1a28a1b8bd8fdb7084e45

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -48,7 +48,7 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 
 // 地址32位 = 19位tags + 7位sets + 6位块内偏移
 uint32_t cache_read(hwaddr_t addr) { // 返回是set_index的哪个block，如果miss，先处理，再返回
-	// printf("(0x%x", addr);
+	printf("(0x%x", addr);
 	uint32_t tag_now = (addr >> 13) & 0x7ffff;
 	uint32_t set_now = (addr >> 6) & 0x7f;
 	bool hit = false;
