f099cabfa86e681106fe5fa9712ebcc12bf80c50
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-18 23:59:12 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.8.0-32-generic #34~20.04.1-Ubuntu SMP Wed Dec 2 21:07:47 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:59:12 up  4:20,  1 user,  load average: 0.45, 0.54, 0.58 c6f70dae3cecbfcb4f1c0057b27b79f497089641

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -16,17 +16,16 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 	int32_t set_l1 = (addr >> l1_sets_bit) & (l1_sets - 1);
 	int32_t i = l1_read(addr);
 	int32_t imm_l1 = (addr & (block_size - 1));
-	int8_t tmp[block_size] = {};														// 把得到的len长度的内容存tmp里（长度为变量len不通过）
+	int8_t tmp[block_size] = {};			// 把得到的len长度的内容存tmp里（长度为变量len不通过）
 	if(imm_l1 + len > block_size) {													/* 跨了两个块 */
 		// 第2个块的地址翻译
 		uint32_t set_last = ((addr + len) >> l1_sets_bit) & (l1_sets - 1);
-		uint32_t i_last = l1_read(addr + len);
 		memcpy(tmp, l1_cache[set_l1][i].block + imm_l1, block_size - imm_l1);									// 复制第一个块的内容
+		uint32_t i_last = l1_read(addr + len);
 		memcpy(tmp + block_size - imm_l1, l1_cache[set_last][i_last].block, len - (block_size - imm_l1));		// 复制剩下的第2个块
 	}
-	else memcpy(tmp, l1_cache[set_l1][i].block + imm_l1, len);					/* 一个块 */
-	int qwq = 0;
-	return unalign_rw(tmp + qwq, 4) & (~0u >> ((4 - len) << 3));						//	在nemu/include/macro.h
+	else memcpy(tmp, l1_cache[set_l1][i].block + imm_l1, len);						/* 一个块 */
+	int qwq = 0; return unalign_rw(tmp + qwq, 4) & (~0u >> ((4 - len) << 3));					//	在nemu/include/macro.h
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
