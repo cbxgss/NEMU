4dae16f6326d849c72aa63cd2eb8c8d94201328a
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-08 17:24:33 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.8.0-32-generic #34~20.04.1-Ubuntu SMP Wed Dec 2 21:07:47 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:24:33 up  2:31,  1 user,  load average: 0.47, 0.57, 0.61 3d0920ab20f8848abcc5b2333adae60875757eee

--- nemu/src/cpu/exec/data-control/jmp-template.h
+++ nemu/src/cpu/exec/data-control/jmp-template.h
@@ -21,13 +21,9 @@ make_instr_helper(i)
 make_instr_helper(rm)
 
 #if DATA_BYTE == 4
-extern Sreg_info sreg_info;
-Sreg_info tmp;
 make_helper(ljmp){
-    sreg_info = tmp;            // 初始化sreg_info，全0
     cpu.eip = instr_fetch(cpu.eip + 1, 4) - 7;                  // 后面eip会+7，所以先-7
     cpu.CS.selector = instr_fetch(cpu.eip + 1 + 4, 2);          // 设置CS寄存器
-    // sreg_set(R_CS);             // 设置CS寄存器
     print_asm("ljmp 0x%x 0x%x",instr_fetch(cpu.eip + 1 + 4, 2), instr_fetch(cpu.eip+1, 4));
     return 7;
 }
