241c65a4f6770292b7bdde93b95647f3447e4ce2
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-15 20:26:10 +0800

    >  compile NEMU 3019244237 chengrong Linux chengrong-Legion-Y7000-2019-PG0 5.8.0-32-generic #34~20.04.1-Ubuntu SMP Wed Dec 2 21:07:47 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:26:10 up  1:20,  1 user,  load average: 1.61, 1.41, 1.02 117093845ac4fa05a8c452565c4d74b6f137880

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -55,7 +55,7 @@ static int cmd_info(char *args){
 		for(i = 0; i < 4; i++){
 			printf("%s : 0x%x\t%s: 0x%x\t", regsb[i], cpu.gpr[i]._8[0], regsb[i+4], cpu.gpr[i]._8[1]);
 		} puts("");
-		printf("eip : 0x%x\n", cpu.eip);
+		printf("eip: 0x%x\n", cpu.eip);
 		printf("GDTR:\tbase: 0x%x\tlimit: 0x%x\n", cpu.GDTR.base, cpu.GDTR.limit);
 	}
 	else if(*args == 'w'){
