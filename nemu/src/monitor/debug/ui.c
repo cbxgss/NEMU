9875bbe4387ad7984fead8ec6c31cc7163dfa0c2
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 21:17:05 +0800

    >  compile NEMU 3019244237 chengrong Linux legion-y7000-2019-pg0 5.8.0-32-generic #34~20.04.1-Ubuntu SMP Wed Dec 2 21:07:47 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:17:05 up  2:09,  1 user,  load average: 1.41, 0.75, 0.54 4a2ffb5cc145b00a4bdbc023bcc182e391f23b5

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -56,7 +56,7 @@ static int cmd_info(char *args){
 			printf("%s : 0x%x\t%s : 0x%x\t", regsb[i], cpu.gpr[i]._8[0], regsb[i+4], cpu.gpr[i]._8[1]);
 		} puts("");
 		printf("eip: 0x%x\n", cpu.eip);
-		printf("GDTR:\tbase: 0x%x\tlimit: 0x%x\n", cpu.GDTR.base, cpu.GDTR.limit);
+		printf("GDTR:\tbase: 0x%x\tlimit: 0x%x\t\t|\t\t", cpu.GDTR.base, cpu.GDTR.limit);
 		printf("page dictionary:\tbase: 0x%x\n", cpu.cr3.page_directory_base);
 	}
 	else if(*args == 'w'){
