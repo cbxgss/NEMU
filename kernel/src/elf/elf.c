12f4c44656b15a6fede6661f0f359e9aa2bb03aa
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-16 20:59:44 +0800

    >  compile kernel 3019244237 chengrong Linux legion-y7000-2019-pg0 5.8.0-32-generic #34~20.04.1-Ubuntu SMP Wed Dec 2 21:07:47 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:59:44 up  1:52,  1 user,  load average: 0.66, 0.70, 0.51 1a2f2cb85a6d0194288c0b2392a128e63e8e7aac

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -43,17 +43,19 @@ uint32_t loader() {
 	for(i = 0; i < elf->e_phnum ; i++, ph++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
+			ph -> p_vaddr = mm_malloc(ph -> p_vaddr,ph -> p_memsz);					// 为进程分配虚拟空间
 
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			ph -> p_vaddr = mm_malloc(ph -> p_vaddr,ph -> p_memsz);					// 为进程分配虚拟空间
 			ramdisk_read ((void *)ph->p_vaddr, ph->p_offset, ph->p_filesz);
 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
 			memset ((void *)(ph->p_vaddr+ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+
+			ph++;
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
