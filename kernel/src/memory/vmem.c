394b9970c96483898455aa44c4a75dc2f6f0de75
tracer-NEMU2020 <tjuics2020@163.com>
2021-01-17 18:38:56 +0800

    >  compile kernel 3019244237 chengrong Linux legion-y7000-2019-pg0 5.8.0-32-generic #34~20.04.1-Ubuntu SMP Wed Dec 2 21:07:47 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:38:56 up  7:28,  1 user,  load average: 0.44, 0.70, 0.86 6290f3a03dcc7f3c3de1c5bacfc41be3deb7ee7b

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -18,12 +18,12 @@ void create_video_mapping() {
 	// panic("please implement me");
 	PDE *pdir = (PDE *)va_to_pa(get_updir());
 	pdir[0].val = make_pde(va_to_pa(ptable));
-	uint32_t i, st, ed;
-	st = VMEM_ADDR / PAGE_SIZE;
-	ed = st + SCR_SIZE / PAGE_SIZE;
-	if(SCR_SIZE % PAGE_SIZE) ed++;
+	uint32_t i, start, end;
+	start = VMEM_ADDR / PAGE_SIZE;
+	end = start + SCR_SIZE / PAGE_SIZE;
+	if(SCR_SIZE % PAGE_SIZE) end++;
 	// fill PTEs
-	for (i = st; i <= ed; i++) {
+	for (i = start; i <= end; i++) {
 		ptable[i].val = make_pte(i << 12);
 	}
 }
